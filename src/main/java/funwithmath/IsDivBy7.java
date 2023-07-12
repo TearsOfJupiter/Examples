0df2bac2ca4a8fb0860f6aa7e9393204171aaea0
Anthony <6005536+TearsOfJupiter@users.noreply.github.com>
2023-07-11 22:45:46 -0600

    - Added funwithmath package and IsDivBy7

---
+++ src/main/java/funwithmath/IsDivBy7.java
@@ -0,0 +1,37 @@
+package funwithmath;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+public class IsDivBy7
+{
+  public static void main(String[] args)
+  {
+    printIsDivBy7(3);
+    printIsDivBy7(49);
+    printIsDivBy7(63);
+    printIsDivBy7(213);
+    printIsDivBy7(210);
+    printIsDivBy7(35864);
+    IntStream.range(0, 10)
+        .map(i -> ThreadLocalRandom.current()
+            .nextInt(0, 1000))
+        .forEach(IsDivBy7::printIsDivBy7);
+  }
+
+  private static void printIsDivBy7(final int num)
+  {
+    System.out.println(num + " divBy7? " + (isDivBy7(num) && num % 7 == 0) + ", " + num + " % 7 == " + num % 7);
+  }
+
+  private static boolean isDivBy7(final int num)
+  {
+    if (num < 7)
+      return false;
+
+    final int calc = 5 * (num % 10) + (num / 10);
+    return calc >= 7
+        && (calc == 7 || calc == num || // 7 ^ (2n)
+            isDivBy7(calc));
+  }
+}
