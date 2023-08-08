b08363b9ef38a1431aba3d5e1c4a482fbdec66a2
Anthony <6005536+TearsOfJupiter@users.noreply.github.com>
2023-08-07 20:58:48 -0600

    - Refactor Unchecker

--- src/main/java/functions/util/Unchecker.java
+++ src/main/java/functions/util/Unchecker.java
@@ -2,13 +2,8 @@ package functions.util;
 
 public class Unchecker
 {
-  public static <E extends Throwable> RuntimeException uncheck(final E e)
+  public static <E extends Throwable> RuntimeException asUnchecked(final E e)
   {
     return new RuntimeException(e);
-  }
-
-  public static <E extends Throwable> void throwUnchecked(final E e)
-  {
-    throw new RuntimeException(e);
   }
 }
