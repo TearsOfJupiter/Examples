60269333dbf73ac48e3bcb5793d606fadd026468
Anthony <6005536+TearsOfJupiter@users.noreply.github.com>
2023-08-10 22:39:00 -0600

    - Added reference link

--- src/main/java/functions/util/tuples/Pair.java
+++ src/main/java/functions/util/tuples/Pair.java
@@ -29,7 +29,7 @@ public class Pair<A, B>
     if (this == obj)
       return true;
 
-    return (obj instanceof Pair<?, ?> p) // Pattern matching, as of 17
+    return (obj instanceof Pair<?, ?> p) // Pattern matching, as of 17 (https://docs.oracle.com/en/java/javase/17/language/pattern-matching-instanceof-operator.html#GUID-843060B5-240C-4F47-A7B0-95C42E5B08A7)
         && Objects.equals(a, p.a)
         && Objects.equals(b, p.b);
   }
