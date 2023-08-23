439f9d8de7cf4cc1bbe2970a4515689c042a898b
Anthony <6005536+TearsOfJupiter@users.noreply.github.com>
2023-08-22 20:22:17 -0600

    - Add missing test coverage

--- src/test/java/parsing/BooleanAlgebraParseTreeTest.java
+++ src/test/java/parsing/BooleanAlgebraParseTreeTest.java
@@ -3,6 +3,7 @@ package parsing;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class BooleanAlgebraParseTreeTest
 {
@@ -24,5 +25,24 @@ public class BooleanAlgebraParseTreeTest
     final BooleanAlgebraParseTree tree = BooleanAlgebraParseTree.parse(relation)
         .orElseThrow();
     assertEquals(tree.toRelationString(), relation);
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void test_parse_invalidOperator()
+  {
+    BooleanAlgebraParseTree.parse("1 blah 2");
+  }
+
+  @Test
+  public void test_parse_empty()
+  {
+    assertTrue(BooleanAlgebraParseTree.parse(null).isEmpty());
+  }
+
+  @Test
+  public void test_toString()
+  {
+    final String relationString = "1 and (2 or 3)";
+    assertEquals(BooleanAlgebraParseTree.parse(relationString).orElseThrow().toString(), relationString);
   }
 }
